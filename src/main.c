a2022d95deea72be60cdccf05083f045d0c5cdbb
Hassam <hassamarshad100@gmail.com>
2025-01-16 12:59:08 +0000

    making new custom overlay file and adding nodes. There are comments in overlay file in main.c for understanding better

--- src/main.c
+++ src/main.c
@@ -8,19 +8,16 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* Now we will again access the led node but with more simpler and stramlined way then the previos one. 
-This will be better to use to access 
-For example we will access the led1 which is defined like this in dts
-		led1: led_1 {
-			gpios = <&gpio0 14 GPIO_ACTIVE_LOW>;
-			label = "Green LED 1";
-		};
+/* Now we will make a new node by making a new oevrlay file and adding a new node there.
+For making a new custom overlay file just goto config files on left then Devicetree and under there click create overlay file
+It will automatically name it with your board.
+then add a new node just copy from dts file the led1 node and rename it there with maybe led4
 
-We will be accessiing using spec node as this will get us all port pin number and flags
-we will change the set configure gpio function sto gpio_pin_set_dt where we will use dt at the end in name 
+Here in overlay file if you have assigned a gpio which is already used on some other function then disable that first and use it then
+
 */
 
-#define LED1_NODE			DT_NODELABEL(led1)		// accesing led1 node in the dts
+#define LED4_NODE			DT_NODELABEL(led4)		// accesing led1 node in the dts
 
 
 int main(void)
@@ -29,16 +26,16 @@ int main(void)
 	// const struct device *gpio0_dev = device_get_binding(GPIO_NAME);
 	// then use this port gpio0_dev to access the led pin
 	// gpio_pin_configure(gpio0_dev, LED3_PIN0, GPIO_OUTPUT | LED3_FLAG);
-	static const struct gpio_dt_spec led1_spec = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-	gpio_pin_configure_dt(&led1_spec, GPIO_OUTPUT);
+	static const struct gpio_dt_spec led4_spec = GPIO_DT_SPEC_GET(LED4_NODE, gpios);
+	gpio_pin_configure_dt(&led4_spec, GPIO_OUTPUT);
 
 
 	while (1) {
 
 		printk("Hello World!\n");
-		gpio_pin_set_dt(&led1_spec, 1);
+		gpio_pin_set_dt(&led4_spec, 1);
 		k_msleep(SLEEP_TIME_MS);
-		gpio_pin_set_dt(&led1_spec, 0);
+		gpio_pin_set_dt(&led4_spec, 0);
 		k_msleep(SLEEP_TIME_MS);
 
 	}

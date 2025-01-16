638d5349879659612588799193e7cffbd23d3a25
Hassam <hassamarshad100@gmail.com>
2025-01-16 12:28:57 +0000

    accessing leds nodes using dt spec structure which is more convinent to use and can get all in spec structure. Need to use it with dt functions as explained in code.

--- src/main.c
+++ src/main.c
@@ -8,37 +8,37 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* Now we will use device tree to access the led nodes which are defined already. 
-For example we will access the led3 which is defined like this in dts
-		led3: led_3 {
-			gpios = <&gpio0 16 GPIO_ACTIVE_LOW>;
-			label = "Green LED 3";
+/* Now we will again access the led node but with more simpler and stramlined way then the previos one. 
+This will be better to use to access 
+For example we will access the led1 which is defined like this in dts
+		led1: led_1 {
+			gpios = <&gpio0 14 GPIO_ACTIVE_LOW>;
+			label = "Green LED 1";
 		};
 
+We will be accessiing using spec node as this will get us all port pin number and flags
+we will change the set configure gpio function sto gpio_pin_set_dt where we will use dt at the end in name 
 */
 
-#define LED3_NODE			DT_NODELABEL(led3)	
-#define LED3_GPIO_NODE		DT_PHANDLE_BY_IDX(LED3_NODE,gpios,0)	// this is handling the led node as we did last time like /soc/gpio@50000000/
-#define GPIO_NAME			DEVICE_DT_NAME(LED3_GPIO_NODE)		// this will bind the name for gpio0 node as we defined above.
-															// So now we have this name to be used in code as device 
-#define LED3_PIN0			DT_GPIO_PIN(LED3_NODE, gpios)	// we access pin number here 16
-#define	LED3_FLAG			DT_GPIO_FLAGS(LED3_NODE, gpios)	// here we are accessing the flag to make it high or low based on if its 
-															// being set active high or active low in dts. 
-															//So with this its automatically getting and making high and low
+#define LED1_NODE			DT_NODELABEL(led1)		// accesing led1 node in the dts
+
 
 int main(void)
 {
 	// const struct device *gpio0_dev = device_get_binding("gpio@50000000");		// here we are accessing it directly with gpio address
-	const struct device *gpio0_dev = device_get_binding(GPIO_NAME);
+	// const struct device *gpio0_dev = device_get_binding(GPIO_NAME);
 	// then use this port gpio0_dev to access the led pin
-	gpio_pin_configure(gpio0_dev, LED3_PIN0, GPIO_OUTPUT | LED3_FLAG);
+	// gpio_pin_configure(gpio0_dev, LED3_PIN0, GPIO_OUTPUT | LED3_FLAG);
+	static const struct gpio_dt_spec led1_spec = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+	gpio_pin_configure_dt(&led1_spec, GPIO_OUTPUT);
+
 
 	while (1) {
 
 		printk("Hello World!\n");
-		gpio_pin_set(gpio0_dev, LED3_PIN0, 1);
+		gpio_pin_set_dt(&led1_spec, 1);
 		k_msleep(SLEEP_TIME_MS);
-		gpio_pin_set(gpio0_dev, LED3_PIN0, 0);
+		gpio_pin_set_dt(&led1_spec, 0);
 		k_msleep(SLEEP_TIME_MS);
 
 	}

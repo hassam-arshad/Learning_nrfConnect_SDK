a5eb084d1f05af3c29735f7ce1a8c080e1d0c70d
Hassam <hassamarshad100@gmail.com>
2025-01-16 11:26:13 +0000

    controlling gpio with hal library. Directly accessing the gpio with pin number

--- src/main.c
+++ src/main.c
@@ -1,48 +1,27 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <hal/nrf_gpio.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define GPIO_PIN			NRF_GPIO_PIN_MAP(0,14)
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
+	nrf_gpio_cfg_output(GPIO_PIN);
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		printk("Hello World!\n");
+		nrf_gpio_pin_set(GPIO_PIN);
 		k_msleep(SLEEP_TIME_MS);
+		nrf_gpio_pin_clear(GPIO_PIN);
+		k_msleep(SLEEP_TIME_MS);
+
 	}
 	return 0;
 }
